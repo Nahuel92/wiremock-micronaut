3378186bbdffeeb447994c782b9533a630d8e2f4
Nahuel Rodriguez <12597182+Nahuel92@users.noreply.github.com>
2024-06-06 21:04:04 -0300

    refactor(wiremock-micronaut): Cleaned up code.
    Signed-off-by: Nahuel Rodriguez <12597182+Nahuel92@users.noreply.github.com>

--- wiremock-micronaut/src/test/java/org/nahuelrodriguez/wiremock/micronaut/WireMockConfigurationCustomizerTest.java
+++ wiremock-micronaut/src/test/java/org/nahuelrodriguez/wiremock/micronaut/WireMockConfigurationCustomizerTest.java
@@ -2,18 +2,21 @@ package org.nahuelrodriguez.wiremock.micronaut;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import io.micronaut.http.HttpHeaders;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.client.HttpClient;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @MicronautTest
@@ -30,28 +33,8 @@ import static org.assertj.core.api.Assertions.assertThat;
         ),
 })
 class WireMockConfigurationCustomizerTest {
-    private static final int USER_SERVICE_PORT = findAvailablePort();
-    private static final int TODO_SERVICE_PORT = findAvailablePort();
-
-    private static int findAvailablePort() {
-        try (final var serverSocket = new ServerSocket(0)) {
-            return serverSocket.getLocalPort();
-        } catch (final IOException e) {
-            throw new UncheckedIOException("Failed to find available port", e);
-        }
-    }
-
-    @Test
-    void outputsWireMockLogs() throws IOException, InterruptedException {
-        userService.stubFor(get(urlEqualTo("/test"))
-                .willReturn(aResponse().withHeader("Content-Type", "text/plain").withBody("Hello World!")));
-        try (final HttpClient httpClient = HttpClient.newHttpClient()) {
-            final HttpResponse<String> response = httpClient.send(
-                    HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:" + userService.port() + "/test")).build(),
-                    HttpResponse.BodyHandlers.ofString());
-            assertThat(response.body()).isEqualTo("Hello World!");
-        }
-    }
+    @Inject
+    private HttpClient httpClient;
 
     @InjectWireMock("user-service")
     private WireMockServer userService;
@@ -60,12 +43,42 @@ class WireMockConfigurationCustomizerTest {
     private WireMockServer todoService;
 
     @Test
-    void appliesConfigurationCustomizer() {
-        assertThat(userService.port()).isEqualTo(USER_SERVICE_PORT);
-        assertThat(todoService.port()).isEqualTo(TODO_SERVICE_PORT);
+    @DisplayName("WireMock should return the stubbed response when a request matches the stub conditions")
+    void successOnGettingWireMockStubbedResponse() {
+        // given
+        userService.stubFor(get(urlEqualTo("/test")).willReturn(aResponse()
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN)
+                        .withBody("Hello World!")
+                )
+        );
+
+        // when
+        final var response = httpClient.toBlocking()
+                .exchange("http://localhost:" + userService.port() + "/test", String.class);
+
+        //then
+        assertThat(response.body()).isEqualTo("Hello World!");
+    }
+
+    @Test
+    @DisplayName("WireMock should pick up the new behavior given by a Configuration Customizer if used")
+    void successOnApplyingConfigurationCustomization() {
+        assertThat(userService.port()).isEqualTo(SampleConfigurationCustomizer.USER_SERVICE_PORT);
+        assertThat(todoService.port()).isEqualTo(SampleConfigurationCustomizer.TODO_SERVICE_PORT);
     }
 
     static class SampleConfigurationCustomizer implements WireMockConfigurationCustomizer {
+        private static final int USER_SERVICE_PORT = findAvailablePort();
+        private static final int TODO_SERVICE_PORT = findAvailablePort();
+
+        private static int findAvailablePort() {
+            try (final var serverSocket = new ServerSocket(0)) {
+                return serverSocket.getLocalPort();
+            } catch (final IOException e) {
+                throw new UncheckedIOException("Failed to find available port", e);
+            }
+        }
+
         @Override
         public void customize(final WireMockConfiguration configuration, final ConfigureWireMock options) {
             if (options.name().equals("user-service")) {
