533a64e350880b29ccb14b74a568125fba171959
Nahuel Rodriguez <12597182+Nahuel92@users.noreply.github.com>
2024-06-09 14:13:23 -0300

    test: Using soft assertions for WireMock configuration.
    Signed-off-by: Nahuel Rodriguez <12597182+Nahuel92@users.noreply.github.com>

--- wiremock-micronaut/src/test/java/io/github/nahuel92/wiremock/micronaut/CommonAssertions.java
+++ wiremock-micronaut/src/test/java/io/github/nahuel92/wiremock/micronaut/CommonAssertions.java
@@ -2,27 +2,28 @@ package io.github.nahuel92.wiremock.micronaut;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import io.micronaut.context.env.Environment;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.assertj.core.api.AutoCloseableSoftAssertions;
 
 public class CommonAssertions {
     public static void assertWireMockServerIsConfigured(final WireMockServer wireMockServer,
                                                         final Environment environment, final String property,
                                                         final String portProperty) {
-        assertThat(wireMockServer)
-                .as("Creates WireMock instance")
-                .isNotNull();
-        assertThat(wireMockServer.baseUrl())
-                .as("WireMock baseUrl is set")
-                .isNotNull();
-        assertThat(wireMockServer.port())
-                .as("Sets random port")
-                .isNotZero();
-        assertThat(environment.getProperty(property, String.class))
-                .as("Sets Micronaut property")
-                .contains(wireMockServer.baseUrl());
-        assertThat(environment.getProperty(portProperty, Integer.class))
-                .as("Sets Micronaut port property")
-                .contains(wireMockServer.port());
+        try (final var softly = new AutoCloseableSoftAssertions()) {
+            softly.assertThat(wireMockServer)
+                    .as("Creates WireMock instance")
+                    .isNotNull();
+            softly.assertThat(wireMockServer.baseUrl())
+                    .as("WireMock baseUrl is set")
+                    .isNotNull();
+            softly.assertThat(wireMockServer.port())
+                    .as("Sets random port")
+                    .isNotZero();
+            softly.assertThat(environment.getProperty(property, String.class))
+                    .as("Sets Micronaut property")
+                    .contains(wireMockServer.baseUrl());
+            softly.assertThat(environment.getProperty(portProperty, Integer.class))
+                    .as("Sets Micronaut port property")
+                    .contains(wireMockServer.port());
+        }
     }
 }
